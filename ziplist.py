488831a8f3f571b8c0e32a5beeb57bffb2eef619
OllieRae-Moore <fwjh0382@leeds.ac.uk>
2025-12-12 08:11:29 +0000

    1.10 done

--- ziplist.py
+++ ziplist.py
@@ -1,6 +1,6 @@
 
-# Name:
-# Username:
+# Name: Oliver Rae-Moore
+# Username: fwjh0382
 
 # Lists contents of a zip archive
 # usage: python ziplist.py <zipfile>
@@ -10,15 +10,26 @@
 # Hint: look at the documentation for the "zipfile" module
 
 import sys
+import zipfile
 #from zipfile import ...   # tools in this module can be used
 
 # filename is a command line argument 
 file_name = ""
 
 # Error message: "Usage: python ziplist.py <filename.zip>"
-
+if len(sys.argv) != 2:
+    raise ValueError(f"Usage: python {sys.argv[0]} <filename.zip>")
+file_name = sys.argv[1]
 # Error message: "File not found: python ziplist.py {file_name}"
-
+try:
+    with zipfile.ZipFile(file_name,"r") as inzip:
+        for file in inzip.namelist():
+                print(str(file))
+except FileNotFoundError:    
+    print(f"File not found: {sys.argv[0]} {sys.argv[1]}")
+except zipfile.BadZipFile:
+     print(f"Bad zip file: {sys.argv[0]} {file_name}")
+     
 # Error message: "Bad zip file: python ziplist.py {file_name}"
 # Hint: There is an exception for this defined in the zipfile module
 
